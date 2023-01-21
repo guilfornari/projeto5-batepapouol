ff35ac7ba78e9ed212fb74b7f4972df4e1703b53
Guilfornari <guilfornari@gmail.com>
2023-01-21 12:02:18 -0300

    Implemantado função para modificar mensagens dependendo do type e também implementado função para enviar o userName para o servidor

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 let messagesChat = [];
+let userNameObject = { name: "" };
 
 function getMessagesFromTheServer() {
     const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
@@ -9,6 +10,7 @@ function getData(response) {
     messagesChat = response.data;
     startChat();
     console.log(messagesChat);
+    showLastMessage();
 }
 
 function startChat() {
@@ -17,10 +19,61 @@ function startChat() {
 
 function printText(message) {
     let messageBox = document.querySelector('ul');
-    messageBox.innerHTML += `<li>(${message.time}) ${message.from} para ${message.to} ${message.text}</li>`;
+    if (message.type === "private_message") {
+        messageBox.innerHTML += `
+        <li class="private_message">
+            <span>(${message.time})</span>
+            <span>${message.from}</span>reservadamente para
+            <span>${message.to}:</span>${message.text}
+        </li>`;
+
+    } else if (message.type === "status") {
+        messageBox.innerHTML += `
+        <li class="status">
+            <span>(${message.time})</span>
+            <span> ${message.from}</span>${message.text}
+        </li>`;
+    } else {
+        messageBox.innerHTML += `
+        <li>
+            <span>(${message.time})</span>
+            <span>${message.from}</span>para
+            <span>${message.to}:</span>${message.text}
+        </li>`;
+    }
 }
 
+let element = "";
+function showLastMessage() {
+    element = document.querySelector("li:last-of-type");
+    element.scrollIntoView();
+}
+
+function createUsername() {
+    let userName = prompt("Qual é o teu lindo nome, snowflakes?")
+    userNameObject = { name: userName };
+    const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', userNameObject);
+    promise.then(logIn);
+    promise.catch(logInProblem);
+}
+
+function logIn(response) {
+    console.log(response);
+}
+
+function logInProblem(error) {
+    console.log(error)
+    if (error.response.status === 400) {
+        createUsername();
+    }
+}
+
+createUsername();
 getMessagesFromTheServer();
+//setInterval(getMessagesFromTheServer, 3000);
+
+
+
 
 
 
